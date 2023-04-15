203bcf305993d7abb0a24b25ea491a226b41f991
Muhammad Zaryab Khan <88337527+zaryabkhan864@users.noreply.github.com>
2023-04-15 12:17:15 +0300

    patient api is working

--- src/app.js
+++ src/app.js
@@ -3,5 +3,8 @@ const app = express()
 app.use(express.json());
 //Import all routes
 const test = require('./routes/test')
+const patient = require('./routes/patientRoute')
+
+app.use('/api/v1', patient)
 app.use('/api/v1', test)
 module.exports = app
