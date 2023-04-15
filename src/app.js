232da107d856d57051bd4ef34bbca4cc7cebc54c
Muhammad Zaryab Khan <88337527+zaryabkhan864@users.noreply.github.com>
2023-04-15 12:49:05 +0300

    tensorflow and xlx configure

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 app.use(express.json());
 //Import all routes
+const predict = require('./routes/bpmRoute')
 const test = require('./routes/test')
 const patient = require('./routes/patientRoute')
 
 app.use('/api/v1', patient)
+app.use('/api/v1', predict);
 app.use('/api/v1', test)
 module.exports = app
