97721614113b1e4b5d23a8894f49eb13ebaba35a
Muhammad Zaryab Khan <88337527+zaryabkhan864@users.noreply.github.com>
2023-04-15 13:33:57 +0300

    patient login api is working

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
+const errorMiddleware = require('./middlewares/erros')
 app.use(express.json());
+app.use(bodyParser.json());
+// const app = express()
+// app.use(express.json());
 //Import all routes
 const predict = require('./routes/bpmRoute')
 const test = require('./routes/test')
@@ -11,4 +16,7 @@ app.use('/api/v1', patient)
 app.use('/api/v1', doctor)
 app.use('/api/v1', predict);
 app.use('/api/v1', test)
+
+//Middle to handle error
+app.use(errorMiddleware);
 module.exports = app
