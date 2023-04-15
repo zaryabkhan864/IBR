3f4ea6e9e2a874bb6fb5d9236ed4522fb972cb87
Muhammad Zaryab Khan <88337527+zaryabkhan864@users.noreply.github.com>
2023-04-15 13:02:00 +0300

    doctor controller is working

--- src/app.js
+++ src/app.js
@@ -5,8 +5,10 @@ app.use(express.json());
 const predict = require('./routes/bpmRoute')
 const test = require('./routes/test')
 const patient = require('./routes/patientRoute')
+const doctor = require('./routes/doctorRoute');
 
 app.use('/api/v1', patient)
+app.use('/api/v1', doctor)
 app.use('/api/v1', predict);
 app.use('/api/v1', test)
 module.exports = app
